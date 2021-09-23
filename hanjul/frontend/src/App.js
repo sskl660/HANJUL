afb9ef7f2917348de80e258a34988b4469953ddf
heeeunk <heeeunk777@gmail.com>
2021-09-23 11:59:08 +0900

    build(Router & Skeleton)
    Former-commit-id: b81d6355fa69b0cc1c34a1b785de5eec4bc89ca6

--- hanjul/frontend/src/App.js
+++ hanjul/frontend/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Hanjul from './Hanjul';
+import Recommend from './Recommend';
+import Traces from './Traces';
+import User from './User';
+import BookDetail from './BookDetail';
+import MyLibrary from './MyLibrary';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <BrowserRouter>
+        {/* 원하는 주소와 component를 설치해줘야합니다. exact는 공통으로 겹치는 home에만 */}
+          <Route path="/" component={Hanjul} exact />
+          <Route path="/recommend" component={Recommend} />
+          <Route path="/traces" component={Traces} />
+          <Route path="/book-detail" component={BookDetail} />
+          <Route path="/mylibrary" component={MyLibrary} />
+          <Route path="/user" component={User} />
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ hanjul/frontend/src/Traces.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Traces extends Component {
+  render () {
+    return (
+      <div>
+        <h1>추천로그가 뜨는 발자취페이지입니다.</h1>
+      </div>
+    )
+  }
+}
+
+export default Traces
