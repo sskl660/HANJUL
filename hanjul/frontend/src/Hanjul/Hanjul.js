5ce471b549591446de60e1ada9230eab7384f8a3
안기훈 <dksdmlwjd1@naver.com>
2021-09-23 23:38:12 +0900

    design: Update hanjul design
    '한 줄' 페이지 디자인 완성
    통신 관련 코드가 필요함
    
    
    Former-commit-id: 18cc74f505edb1da45e51cbd1b535a8926efc82f

--- hanjul/frontend/src/Hanjul/Hanjul.js
+++ hanjul/frontend/src/Hanjul/Hanjul.js
@@ -5,7 +5,7 @@ function Home() {
     <div className="hanjul">
       <p className="hanjul-title">ㅎㅏㄴㅈㅜㄹ</p>
       <div className="hanjul-input">
-        <input type="text" placeholder="입력" autoFocus/>
+        <input type="text" placeholder="한 줄을 입력해주세요" autoFocus/>
         <i class="fas fa-search"></i>
       </div>
         <h2 className="hanjul-description">"당신의 인생을 한 줄로 표현한다면?"</h2>
