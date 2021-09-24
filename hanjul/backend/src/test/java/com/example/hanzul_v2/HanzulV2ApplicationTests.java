c02d92ad3e0ea6142099ea0ced25301fce7f3f0d
Kimtaehyun <sskl600@naver.com>
2021-09-24 16:32:21 +0900

    Update : Backend build test code update
    Former-commit-id: 62a429f2d46ec166e2ed63454b69533f20fef430

--- hanjul/backend/src/test/java/com/example/hanzul_v2/HanzulV2ApplicationTests.java
+++ hanjul/backend/src/test/java/com/example/hanzul_v2/HanzulV2ApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.hanzul_v2;
 
+import com.hanzul_v2.HanzulV2Application;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = HanzulV2Application.class)
 class HanzulV2ApplicationTests {
 
     @Test
