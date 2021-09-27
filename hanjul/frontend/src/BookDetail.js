2a243bb8bc5a0b934326abe21892f86d56e8833a
차은채 <jjcec@naver.com>
2021-09-27 15:32:20 +0900

    Design : bookDetail pagination
    Former-commit-id: f83e0df7b59b4210f48cf0aff651855ce49b2449

--- hanjul/frontend/src/BookDetail.js
+++ hanjul/frontend/src/BookDetail.js
@@ -1,65 +1,73 @@
-import { Component } from "react";
-import './components/BookDetail/BookDetail.css'
+import { useState } from "react";
+import './components/BookDetail/BookDetail.css';
 
-class BookDetail extends Component {
-  render () {
-    return (
-      <div className="bookDetail">
-        <div>
-          <button class="bookmark">
-              <svg viewBox="0 0 36 36">
-                  <path class="filled" d="M26 6H10V18V30C10 30 17.9746 23.5 18 23.5C18.0254 23.5 26 30 26 30V18V6Z" />
-                  <path class="default" d="M26 6H10V18V30C10 30 17.9746 23.5 18 23.5C18.0254 23.5 26 30 26 30V18V6Z" />
-                  <path class="corner" d="M10 6C10 6 14.8758 6 18 6C21.1242 6 26 6 26 6C26 6 26 6 26 6H10C10 6 10 6 10 6Z" />
-              </svg>
-          </button>
-          {/* <a class="dribbble" href="https://dribbble.com/ai" target="_blank"><img src="https://cdn.dribbble.com/assets/dribbble-ball-mark-2bd45f09c2fb58dbbfb44766d5d1d07c5a12972d602ef8b32204d28fa3dda554.svg" alt=""></img></a> */}
-        </div>
-        <div className="bookD-info d-flex justify-content-center">
-          <div className="bookD-infoL">
-            <div className="bookD-bookImg">
-              <img src="" alt="" />
-            </div>
-            <div className="bookD-bookGrade">
-              <h3>5점</h3>
-            </div>
+function BookDetail (){
+  const [bookmark, setBookmark] = useState(false);
+  const handleBookmark = () => {
+    setBookmark(!bookmark);
+  }
+  return (
+    <div className="bookDetail">
+      <div className="bookmarkIcon" onClick={handleBookmark}>
+        {bookmark ? (<i className="far fa-bookmark fa-3x bookmark-img"></i>)
+          : (<i className="fas fa-bookmark fa-3x bookmark-img"></i>)
+        }
+      </div>
+      <div className="bookD-info d-flex justify-content-center">
+        <div className="bookD-infoL">
+          <div className="bookD-bookImg">
+            <img src="" alt="" />
           </div>
-          <div className="bookD-infoR">
-            <div className="bookD-bookInfo">
-              <h3>도서명</h3>
-            </div>
-            <div className="bookD-bookInfo">
-              <h3>저자</h3>
-            </div>
-            <div className="bookD-bookInfo">
-              <h3>출판사 출판일</h3>
-            </div>
-            <div className="bookD-bookIntro">
-              <h3>소개</h3>
-            </div>
+          <div className="bookD-bookGrade">
+            <h3>5점</h3>
           </div>
         </div>
-        <div className="bookD-recommend">
-          
-        </div>
-        <div className="bookD-review">
-          <p className="bookD-reviewTitle">한줄평</p>
-          <div className="bookD-reviewBox d-flex justify-content-center">
-            <div className="bookD-reviewInput">
-              <input type="text" placeholder="의견을 남겨주세요" autoFocus />
-            </div>
-            <button className="bookD-btn">입력</button>
+        <div className="bookD-infoR">
+          <div className="bookD-bookInfo">
+            <h3>도서명</h3>
           </div>
-          <div className="bookD-reviewShow">
-            
+          <div className="bookD-bookInfo">
+            <h3>저자</h3>
           </div>
-          <div className="bookD-pagination">
-            <h2>pagination</h2>
+          <div className="bookD-bookInfo">
+            <h3>출판사 출판일</h3>
+          </div>
+          <div className="bookD-bookIntro">
+            <h3>소개</h3>
           </div>
         </div>
       </div>
-    )
-  }
+      <div className="bookD-recommend">
+        
+      </div>
+      <div className="bookD-review">
+        <p className="bookD-reviewTitle">한줄평</p>
+        <div className="bookD-reviewBox d-flex justify-content-center">
+          <div className="bookD-reviewInput">
+            <input type="text" placeholder="의견을 남겨주세요" autoFocus />
+          </div>
+          <button className="bookD-btn">입력</button>
+        </div>
+        <div className="bookD-reviewShow">
+          
+        </div>
+        <div className="bookD-pagination">
+          <ul className="pagination">
+            <li><a href="#">이전</a></li>
+            <li><a href="#">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+            <li><a href="#">5</a></li>
+            <li><a href="#">6</a></li>
+            <li><a href="#">7</a></li>
+            <li><a href="#">8</a></li>
+            <li><a href="#">다음</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default BookDetail
