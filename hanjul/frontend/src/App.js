4dd0fcd7f5edba4d70a011a173e9bfc659a22ee4
차은채 <jjcec@naver.com>
2021-10-05 20:37:48 +0900

    Axios : bookDetail & myLibrary
    Former-commit-id: 90d4b8bf239cc874f89e1b4c90ad4dd98ade0d6c

--- hanjul/frontend/src/App.js
+++ hanjul/frontend/src/App.js
@@ -7,19 +7,22 @@ import User from './User';
 import BookDetail from './BookDetail';
 import MyLibrary from './MyLibrary';
 import Navbar from './components/Navbar/Navbar';
+import ScrollToTop from './ScrollToTop';
 
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
-        <Navbar />
-      {/* 원하는 주소와 component를 설치해줘야합니다. exact는 공통으로 겹치는 home에만 */}
-        <Route path="/" component={Hanjul} exact />
-        <Route path="/recommend" component={Recommend} />
-        <Route path="/history" component={History} />
-        <Route path="/book-detail" component={BookDetail} />
-        <Route path="/mylibrary" component={MyLibrary} />
-        <Route path="/user" component={User} />
+        <ScrollToTop>
+          <Navbar />
+        {/* 원하는 주소와 component를 설치해줘야합니다. exact는 공통으로 겹치는 home에만 */}
+          <Route path="/" component={Hanjul} exact />
+          <Route path="/recommend" component={Recommend} />
+          <Route path="/history" component={History} />
+          <Route path="/book-detail/:bookIsbn" component={BookDetail} />
+          <Route path="/mylibrary" component={MyLibrary} />
+          <Route path="/user" component={User} />
+        </ScrollToTop>
       </BrowserRouter>
     </div>
   );
